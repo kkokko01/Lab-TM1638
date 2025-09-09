00cd1100040b62d61aa2899639fde9c07ca9c16b
kkokko01 <dsgantivao@udistrital.edu.co>
2025-09-08 21:39:44 -0500

    Contador.py

--- Contador.py
+++ Contador.py
@@ -1,4 +1,3 @@
-# Contador en TM1638 
 from TM1638 import TM1638
 import machine
 import time
@@ -13,17 +12,39 @@ contador = 0
 direccion = 1
 MAX_NUM = 99999999
 
+# Configuración de antirrebote
+DEBOUNCE_MS = 200
+last_state = 0
+last_time = [0] * 8   # un tiempo por cada botón
+
 while True:
     tm.number(contador)
-    time.sleep(0.2)
+    time.sleep(0.05)  # refresco rápido
+
     keys = tm.keys()
-    if keys & 0x01:         # Si el primer botón está presionado, resetea
-        contador = 0
-    elif keys:
-        direccion *= -1     # Cualquier otro botón cambia dirección
+    now = time.ticks_ms()
+
+    for i in range(8):  # recorrer cada botón
+        mask = 1 << i
+        pressed_now = keys & mask
+        pressed_before = last_state & mask
+
+        # flanco de subida con antirrebote por botón
+        if pressed_now and not pressed_before:
+            if time.ticks_diff(now, last_time[i]) > DEBOUNCE_MS:
+                # --- acción según botón ---
+                if i == 0:
+                    contador = 0           # Botón 0 = reset
+                else:
+                    direccion *= -1        # Otros botones = cambia dirección
+                # -------------------------
+                last_time[i] = now
+
+    last_state = keys
+
+    # Actualizar contador
     contador += direccion
     if contador < 0:
         contador = MAX_NUM
     elif contador > MAX_NUM:
-
         contador = 0
